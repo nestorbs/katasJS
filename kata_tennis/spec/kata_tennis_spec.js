00ca7d5331ecf9761cdeca59b5e7ced2d7293526
Nestor Bethencourt <nestorbs@gmail.com>
2012-11-23 11:54:48 +0000

    Empezando katatennis

--- kata_tennis/spec/kata_tennis_spec.js
+++ kata_tennis/spec/kata_tennis_spec.js
@@ -1,33 +1,67 @@
 describe("kata tennis", function(){
 
 	it("puntua jugador", function(){
-		var jugador = new Jugador();
+		var jugador = new Jugador(0);
 		expect(jugador.puntua()).toEqual(15);
 		expect(jugador.puntua()).toEqual(30);
 		expect(jugador.puntua()).toEqual(40);
 	});
 
-	it("ventaja", function(){
+	it("jugador consigue ventaja", function(){
 		var jugador = new Jugador();
-		jugador.setVentaja();
-		expect(jugador.getVentaja()).toEqual(true);
+		jugador.setVentaja(true);
+		expect(jugador.tieneVentaja()).toEqual(true);
 	});
 
-	it("iguales 'deuce' ", function(){
-		var jugador = new Jugador();
-		jugador.setVentaja();
-		jugador.iguales();
-		expect(jugador.getVentaja()).toEqual(false);
+	it("jugador 1 gana juego a jugador 2 con marcador de juego 40 / 30", function(){
+		var partido = new Partido([0,0], 40, 30);
+		partido.punto(1);
+		expect(partido.getSet()).toEqual([1,0]);
 	});
 
-	it("jugador 1 gana a jugador 2 con marcador 40 / 30", function(){
-		var jugador1 = new Jugador();
-		var jugador2 = new Jugador();
-		jugador2.puntua();
-		jugador2.puntua();
-		jugador1.puntua();
-		jugador1.puntua();
-		jugador1.puntua();
-		jugador1.puntua();
+	it("jugador 2 gana juego a jugador 1 con marcador de juego 30 / 40", function(){
+		var partido = new Partido([0,0], 30, 40);
+		partido.punto(2);
+		expect(partido.getSet()).toEqual([0,1]);
+	});
+
+	it("jugador 1 puntua con marcador de juego 30/40 alcanzado el juego iguales deuce", function(){
+		var partido = new Partido([0,0], 30, 40);
+		partido.punto(1);
+		expect(partido.getSet()).toEqual([0,0]);
+		expect(partido.getMarcadorJuego()).toEqual("IGUALES");
+	});
+
+	it("jugador 2 puntua con marcador de juego 40/30 alcanzado el juego iguales deuce", function(){
+		var partido = new Partido([0,0], 40, 30);
+		partido.punto(2);
+		expect(partido.getSet()).toEqual([0,0]);
+		expect(partido.getMarcadorJuego()).toEqual("IGUALES");
+	});
+
+	it("jugador 1 puntua tras estar el marcador de juego en iguales", function(){
+		var partido = new Partido([0,0], 40, 40);
+		partido.punto(1);
+		expect(partido.getSet()).toEqual([0,0]);
+		expect(partido.getMarcadorJuego()).toEqual("VENTAJA JUGADOR 1");
+	});
+
+	it("jugador 1 puntua dos veces, una para tener ventaja y otra para ganar juego", function(){
+		var partido = new Partido([0,0], 40, 40);
+		partido.punto(1);
+		partido.punto(1);
+		expect(partido.getSet()).toEqual([1,0]);
+		expect(partido.getMarcadorJuego()).toEqual("0,0");
+	});
+
+	it("jugador 2 puntua dos veces, una para tener ventaja y otra para ganar juego", function(){
+		var partido = new Partido([0,0], 40, 40);
+		alert(partido.getSet());
+		partido.punto(2);
+		alert(partido.getSet());
+		partido.punto(2);
+		alert(partido.getSet());
+		expect(partido.getSet()).toEqual([0,1]);
+		expect(partido.getMarcadorJuego()).toEqual("0,0");
 	});
 });
--- kata_tennis/src/kata_tennis.js
+++ kata_tennis/src/kata_tennis.js
@@ -1,5 +1,5 @@
-function Jugador(){
-	this.puntuacion = 0;
+function Jugador(puntuacionInicial){
+	this.puntuacion = puntuacionInicial;
 	this.ventaja = false;
 }
 
@@ -16,15 +16,85 @@ Jugador.prototype = {
 		}
 		return this.puntuacion;
 	},
-	setVentaja: function(){
-		this.ventaja = true;
+	setVentaja: function(ventaja){
+		this.ventaja = ventaja;
+	},
+	tieneVentaja: function(){
 		return this.ventaja;
 	},
-	getVentaja: function(){
-		return this.ventaja;
+	getPuntuacion: function(){
+		return this.puntuacion;
 	},
-	iguales: function(){
-		this.ventaja = false;
-		return this.ventaja;
+	setPuntuacion: function(puntos){
+		return this.puntuacion = puntos;
 	}
 };
+
+function Partido(setInicial, puntuacionInicialJugador1, puntuacionInicialJugador2){
+	this.set = setInicial;
+	this.jugador1 = new Jugador(puntuacionInicialJugador1);
+	this.jugador2 = new Jugador(puntuacionInicialJugador2);
+	this.JUGADOR1 = 1;
+	this.JUGADOR2 = 2;
+}
+
+Partido.prototype = {
+	constructor: Partido,
+	getSet: function(){
+		return this.set;
+	},
+	punto: function(numeroJugador){
+		if(numeroJugador == this.JUGADOR1){
+			if(this.jugador1.getPuntuacion() == 40){
+				if(this.jugador2.getPuntuacion() == 40){
+					if(this.jugador1.tieneVentaja()){
+						this.set[0]++;
+						this.iniciaJuego();
+					}else{
+						this.jugador1.setVentaja(true);
+					}
+				}else{
+					this.set[0]++;
+					this.iniciaJuego();
+				}
+			}else{
+				this.jugador1.puntua();
+			}
+		}else{
+			if(this.jugador2.getPuntuacion() == 40){
+				if(this.jugador1.getPuntuacion() == 40){
+					if(this.jugador2.tieneVentaja()){
+						this.set[1]++;
+						this.iniciaJuego();
+					}else{
+						this.jugador2.setVentaja(true);
+					}
+				}else{
+					this.set[1]++;
+					this.iniciaJuego();
+				}	
+			}else{
+				this.jugador2.puntua();
+			}
+		}
+	},
+	getMarcadorJuego: function(){
+		if(this.jugador1.tieneVentaja()){
+			return "VENTAJA JUGADOR 1";
+		}
+		if(this.jugador2.tieneVentaja()){
+			return "VENTAJA JUGADOR 2";
+		}
+		if(this.jugador1.getPuntuacion() == 40 && this.jugador2.getPuntuacion() == 40){
+			return "IGUALES";
+		}
+		return this.jugador1.getPuntuacion().toString() + "," + this.jugador2.getPuntuacion().toString();
+	},
+	iniciaJuego: function(){
+		this.jugador1.setPuntuacion(0);
+		this.jugador2.setPuntuacion(0);
+		this.jugador1.setVentaja(false);
+		this.jugador2.setVentaja(false);
+	}
+	
+}
