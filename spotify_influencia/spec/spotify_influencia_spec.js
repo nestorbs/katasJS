2f47fe789f37782233bd743fcbc9920afe6516b6
Nestor Bethencourt <nestorbs@gmail.com>
2012-12-14 09:40:28 +0000

    spotify_influencia: empezando la kata

--- spotify_influencia/spec/spotify_influencia_spec.js
+++ spotify_influencia/spec/spotify_influencia_spec.js
@@ -1,19 +1,20 @@
 describe("Spotify influencia", function(){
+	var spotify 
+	beforeEach(function(){
+		spotify = new Spotify();
+	});
 
 	it("[1009, 2011] [1017, 2011] should return 2011", function(){
-		var spotify = new Spotify();
 		var teams = new Array ([1009, 2011], [1017, 2011]);
 		expect(spotify.whoGoesToTheBeach(teams)).toEqual([2011]);
 	});
 
 	it("[1009, 2011] [1009, 2020] should return 1009", function(){
-		var spotify = new Spotify();
 		var teams = new Array([1009, 2011], [1009, 2020]);
 		expect(spotify.whoGoesToTheBeach(teams)).toEqual([1009]);
 	});
 
 	it("[1009, 2011] [1009, 2020] should return [1009, 2]", function(){
-		var spotify = new Spotify();
 		var teams = new Array ([1009, 2011], [1009, 2020]);
 		var listPerson1 = new Array();
 		spotify.anadePerson(teams[0][0], listPerson1);
@@ -21,7 +22,6 @@ describe("Spotify influencia", function(){
 	});
 
 	it("Elimina parejas representadas por candidato 1009 [1009, 2011] [1009, 2020] should return [1009]", function(){
-		var spotify = new Spotify();
 		var teams = new Array ([1009, 2011], [1009, 2020]);
 		var listPerson1 = new Array([1009, 2]);
 		var listPerson2 = new Array([2011, 1], [2020, 1]);
@@ -32,12 +32,10 @@ describe("Spotify influencia", function(){
 		expect(teams).toEqual([]);
 	});
 
-	it("ordena Array de mayor a menor segun el segundo campo", function(){
-		var spotify = new Spotify();
+	it("ordena de mayor a menor segun el segundo campo del elemento del vector", function(){
 		var teams = new Array ([1009, 1], [1006, 3], [1005, 2], [1011,2]);
-
+		
 		var result = spotify.ordena(teams);
 		expect(result).toEqual([[1006, 3], [1005, 2], [1011, 2], [1009, 1]]);
-		//expect(teams).toEqual([]);
 	});
 });
